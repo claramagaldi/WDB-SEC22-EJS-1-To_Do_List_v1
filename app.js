d945312a735dd91dbc5d7d4598d9ec27ff6147e9
Clara Magaldi Rodrigues <claramagaldi@gmail.com>
2021-08-22 15:45:46 -0400

    Add dynamic date + CSS

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 // APP //
 const app = express();
 
-// USE //
+//VARs//
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
+// USE //
 app.set('view engine', 'ejs');
-//app.use(express.static("public"));
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -16,40 +18,32 @@ app.use(bodyParser.urlencoded({
 
 // GET //
 app.get("/", function(req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
+  let today = new Date();
+  let currentDay = today.getDay();
+  let day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error! Current day is equal to: " + currentDay)
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
 
+  day = today.toLocaleDateString("en-us", options);
+
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    listItems: items
   });
+
+})
+
+app.post("/", function(req, res) {
+
+  let item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
 
 })
 
